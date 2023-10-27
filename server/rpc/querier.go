be3e6f4c43a37c23e024c5ed797a857d66d552a0
Leo Sjöberg <leo.sjoberg@gmail.com>
2023-10-27 22:29:04 +0100

    Support hosts in querier

--- server/rpc/querier.go
+++ server/rpc/querier.go
@@ -6,6 +6,7 @@ import (
 )
 
 var queriers = make(map[any]any)
+var hosts = make(map[any]string)
 
 type QuerierConstructor[T any] func(cc *grpc.ClientConn) Querier[T]
 
@@ -14,9 +15,26 @@ func RegisterQuerier[T any](querier QuerierConstructor[T]) {
 	queriers[res] = querier
 }
 
+func RegisterHost[T any](host string) {
+	res := *new(T)
+	hosts[res] = host
+}
+
 func NewQuerier[T any](cc *grpc.ClientConn) Querier[T] {
 	res := *new(T)
+
 	return queriers[res].(QuerierConstructor[T])(cc)
+}
+
+func ConnectNewQuerier[T any](ctx context.Context) (Querier[T], error) {
+	res := *new(T)
+
+	cc, err := grpc.DialContext(ctx, hosts[res])
+	if err != nil {
+		return nil, err
+	}
+
+	return queriers[res].(QuerierConstructor[T])(cc), nil
 }
 
 type Querier[T any] interface {
@@ -24,6 +42,10 @@ type Querier[T any] interface {
 }
 
 func Find[T any](ctx context.Context, id string) (*T, error) {
-	querier := NewQuerier[T]()
+	querier, err := ConnectNewQuerier[T](ctx)
+	if err != nil {
+		return nil, err
+	}
+
 	return querier.Find(ctx, id)
 }
